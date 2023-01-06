1c156c95847933df53bb52acc1d029ea9b92fd40
Nomz <nomzisnice@yahoo.com>
2023-01-06 16:26:21 +0100

    nowy format cafe.json

--- src/commands/cafe.js
+++ src/commands/cafe.js
@@ -13,9 +13,9 @@ module.exports = {
 		const things = Object.keys(cafe).filter(thing => !thing.includes('-'));
 		things.forEach(thing => {
 			return menu.push({
-				label: cafe[thing],
-				emoji: cafe[`${thing}-emoji`],
-				description: cafe[`${thing}-desc`],
+				label: cafe[thing].name,
+				emoji: cafe[thing].emoji,
+				description: cafe[thing].description,
 				value: thing,
 			});
 		});
@@ -43,11 +43,11 @@ module.exports = {
 							.addComponents(
 								new ButtonBuilder()
 									.setCustomId(i.values[0])
-									.setLabel(cafe[`${i.values[0]}-use`])
+									.setLabel(cafe[i.values[0]].button)
 									.setStyle('Primary'),
 							);
 
-						await i.followUp({ content: cafe[`${i.values[0]}-give`], files: [attachment], components: [action], fetchReply: true })
+						await i.followUp({ content: cafe[i.values[0]].give, files: [attachment], components: [action], fetchReply: true })
 							.then(inter2 => {
 								const filter2 = i2 => {
 									if (i2.user.id !== interaction.user.id) i2.reply({ content: 'To nie twoje. Nie ruszaj tego!', ephemeral: true });
@@ -56,8 +56,8 @@ module.exports = {
 
 								inter2.awaitMessageComponent({ filter: filter2, componentType: ComponentType.Button, time: 60000 })
 									.then(async i2 => {
-										await i2.update({ content: cafe[`${i2.customId}-using`].replace('[user]', `<@${i2.user.id}>`), components: [] });
-										await i2.followUp({ content: cafe[`${i2.customId}-used`][Math.floor(Math.random() * cafe[`${i2.customId}-used`].length)] });
+										await i2.update({ content: cafe[i2.customId].action.replace('[user]', `<@${i2.user.id}>`), components: [] });
+										await i2.followUp({ content: cafe[i2.customId].after[Math.floor(Math.random() * cafe[i2.customId].after.length)] });
 									})
 									.catch(() => {
 										action.components[0].setDisabled(true).setLabel('No i się przeterminowało').setStyle('Danger');
